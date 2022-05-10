a4226ed7a75e0abeca13457ec7a77b29ad72db54
Abhay Kumar <abhay0480sharma@gmail.com>
2022-05-10 17:20:05 +0530

    meme generator setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <section >
+
+
+      <header className='p-[20px] bg-gradient-to-r from-violet-500 to-fuchsia-500  flex items-center mb-[50px]'>
+      <img src='images/TrollFace.svg' alt='trollface' className='mr-[10px]'/> <span className='text-[30px] text-white font-bold'>Meme Generator</span>
+      <p className='ml-auto text-white'>React Course - Project 3</p>
       </header>
-    </div>
+
+      <div className='px-[400px]'>
+        <form className='mx-auto'>
+          <div className='flex justify-between mb-[30px]'>
+            <input className='py-[10px] px-[10px] border-[1px] border-[#D5D4D8] rounded w-[480px]' text='text' placeholder='Top-text'/>
+            <input className='py-[10px] px-[10px] border-[1px] border-[#D5D4D8] rounded w-[480px]' text='text' placeholder='Bottom-text'/>
+          </div>
+            <button className='py-[15px] px-[10px] border-[1px] border-[#D5D4D8] rounded w-[100%] text-white bg-gradient-to-r from-violet-500 to-fuchsia-500'>Get a new meme image  🖼</button>
+        </form>
+      </div>
+    </section>
   );
 }
 
